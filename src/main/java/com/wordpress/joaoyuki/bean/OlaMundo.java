8466ab6f9266e4a0560f452f6c6bd84df8a8d502
jfassis <joao.yuki@gmail.com>
2017-03-16 14:55:18 -0300

    Atualização nos comentários para que fique mais explicativo.

--- src/main/java/com/wordpress/joaoyuki/bean/OlaMundo.java
+++ src/main/java/com/wordpress/joaoyuki/bean/OlaMundo.java
@@ -3,15 +3,16 @@ package com.wordpress.joaoyuki.bean;
 import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
-//Usamos a anotação @ManagedBean para que essa classe seja gerenciada pelo JSF.
-@ManagedBean
+@ManagedBean //Usamos a anotação @ManagedBean para que essa classe seja gerenciada pelo JSF.
+@ViewScoped  //Usamos a anotação @ViewScoped para que o bean só esteja disponível durante a visualização. 
 public class OlaMundo implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
-	private String mensagem = "Olá mundo";
-
+	private String mensagem = "Olá mundo - Managed bean";
+	
 	//Precisamos criar os métodos getters and setters para que o JSF consiga identificar o atributo nas páginas.
 	public String getMensagem() {
 		return mensagem;
